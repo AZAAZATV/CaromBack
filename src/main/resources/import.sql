03835dbdd898401d8f09124f8f1d2eed08427899
shk0625 <s22042@gsm.hs.kr>
2022-12-23 19:34:55 +0900

    최종본

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into usersave(id,password,name,class) values("dssbsd",1234,"김d희","1-1-3");
+insert into usersave(id,password,name,class) values("sdfsd",24562,"김희","1-1-3");
+insert into usersave(id,password,name,class) values("dssbbsbsd",2141,"김d","1-1-3");
+insert into usersave(id,password,name,class) values("dsq34sdsbsd",2352,"d희","1-1-3");
+insert into usersave(id,password,name,class) values("dssbcxssd",23423,"김","1-1-3");
---
+++ src/test/java/AZAAZTV1/azaaztv1/Azaaztv1ApplicationTests.java
@@ -0,0 +1,9 @@
+package AZAAZTV1.azaaztv1;
+
+class Azaaztv1ApplicationTests {
+
+	void contextLoads() {
+
+	}
+
+}
